c11e766bc0540e53971acc1b94763fd8ab3982c4
Edvin Norling <edvin.norling@xenit.se>
2022-03-21 17:04:27 +0100

    Add http get feature
    Still hardcoded endpoints.
    Added some strucutre but not much...

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dns-debug
 
 A simple app that runs a dns query and traces it.
+
+Don't read the code it's ugly and not mean to be used other then a quick test for me.
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
@@ -15,15 +17,28 @@ func main() {
 	if myurl == "" {
 		myurl = "google.com"
 	}
-	endpoints := []string{"google.com", "google.se"}
+	curl := os.Getenv("CURL")
+	if curl == "" {
+		curl = "true"
+	}
+	curlBool, err := strconv.ParseBool(curl)
+	if err != nil {
+		panic(err)
+	}
+	endpoints := []string{"https://google.com", "https://google.se", "https://gp.se", "https://dn.se", "https://di.se"}
 	fmt.Printf("Endpoints: %v\n", endpoints)
 	fmt.Printf("DNS URL: %v\n", myurl)
-	for _, name := range endpoints {
-		_, err := url.Parse(name)
+	for _, endpoint := range endpoints {
+		_, err := url.Parse(endpoint)
 		if err != nil {
 			panic(err)
 		}
 	}
+	run(myurl, curlBool, endpoints)
+}
+
+func run(myurl string, curlBool bool, endpoints []string) {
+	c := &http.Client{Timeout: time.Duration(2) * time.Second}
 
 	tracer.Start()
 	defer tracer.Stop()
@@ -32,8 +47,17 @@ func main() {
 		if err != nil {
 			break
 		}
+		if curlBool {
+			for _, endpoint := range endpoints {
+				err := getHTTP(endpoint, c)
+				if err != nil {
+					break
+				}
+			}
+		}
 		time.Sleep(5 * time.Second)
 	}
+	fmt.Println("something broke")
 	os.Exit(1)
 }
 
@@ -43,13 +67,29 @@ func lookup(myurl string) error {
 	start := time.Now()
 	ips, err := net.LookupIP(myurl)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not get IPs: %v\n", err)
+		fmt.Printf("Could not get IPs: %v\n", err)
 		return err
 	}
 	duration := time.Since(start)
-	fmt.Printf("This was the duration: %v\n", duration)
+	fmt.Printf("This was the duration for dns: %v\n", duration)
 	fmt.Printf("This is the IP:s %v\n", ips)
 	// Set tag
 	span.SetTag("url", myurl)
+	return nil
+}
+
+func getHTTP(endpoint string, c *http.Client) error {
+	span := tracer.StartSpan("http.request", tracer.ResourceName("http"))
+	defer span.Finish()
+	start := time.Now()
+	resp, err := c.Get(endpoint)
+	if err != nil {
+		fmt.Printf("Unable to get endpoint: %v, got error: %s", endpoint, err)
+		return err
+	}
+	duration := time.Since(start)
+	fmt.Printf("This was the duration for get: %v\n", duration)
+	fmt.Printf("Endpoint: %v status code: %v\n", endpoint, resp.StatusCode)
+	span.SetTag("endpoint", endpoint)
 	return nil
 }
